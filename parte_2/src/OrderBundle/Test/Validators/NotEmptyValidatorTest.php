cdb77585e3d9f5ed8e57ae96933e87125ec3c6f7
Andre Ferreira <andre.ferreira@soluti.com.br>
2020-05-13 10:03:13 -0300

    Implementando data provider

--- parte_2/src/OrderBundle/Test/Validators/NotEmptyValidatorTest.php
+++ parte_2/src/OrderBundle/Test/Validators/NotEmptyValidatorTest.php
@@ -7,23 +7,30 @@ use OrderBundle\Validators\NotEmptyValidator;
 
 class NotEmptyValidatorTest extends TestCase
 {
-    public function testShouldNotBeValidWhenValueIsEmpty()
+    /**
+     * @dataProvider valueProvider
+     */
+    public function testIsValid($value, $expectedResult)
     {
-        $emptyValue = "";
-        $notEmptyValidator = new NotEmptyValidator($emptyValue);
+        $notEmptyValidator = new NotEmptyValidator($value);
 
         $isValid = $notEmptyValidator->isValid();
 
-        $this->assertFalse($isValid);
+        $this->assertEquals($expectedResult, $isValid);          
     }
 
-    public function testShouldBeValidWhenValueIsNotEmpty()
+    public function valueProvider()
     {
-        $notEmptyValue = "foo";
-        $notEmptyValidator = new NotEmptyValidator($notEmptyValue);
-
-        $isValid = $notEmptyValidator->isValid();
-
-        $this->assertTrue($isValid);
+        return [
+            'shouldBeValidWhenValueIsNotEmpty' => [
+                'value' => 'foo', 
+                'expectedResult' => true
+            ],
+            'shouldNotBeValidWhenValueIsEmpty' => [
+                'value' => '', 
+                'expectedResult' => false
+            ]
+        ];
     }
+
 }
