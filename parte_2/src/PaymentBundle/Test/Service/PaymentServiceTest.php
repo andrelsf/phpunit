f12639315262d9e830584fb05a21f1c4dcdb4b24
Andre Ferreira <andre.ferreira@soluti.com.br>
2020-05-26 14:07:29 -0300

    Testando Exceptions

--- parte_2/src/PaymentBundle/Test/Service/PaymentServiceTest.php
+++ parte_2/src/PaymentBundle/Test/Service/PaymentServiceTest.php
@@ -5,6 +5,7 @@ namespace PaymentBundle\Test\Service;
 use OrderBundle\Entity\CreditCard;
 use OrderBundle\Entity\Customer;
 use OrderBundle\Entity\Item;
+use PaymentBundle\Exception\PaymentErrorException;
 use PaymentBundle\Repository\PaymentTransactionRepository;
 use PaymentBundle\Service\Gateway;
 use PaymentBundle\Service\PaymentService;
@@ -30,6 +31,34 @@ class PaymentServiceTest extends TestCase
         
         $paymentTransactionRepository->expects($this->once())
                                      ->method('save');
+
+        $customer = $this->createMock(Customer::class);
+        $item = $this->createMock(Item::class);
+        $creditCard = $this->createMock(CreditCard::class);
+
+        $paymentService->pay($customer, $item, $creditCard);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldThrowExceptionWhenGatewayFails()
+    {
+        $gateway = $this->createMock(Gateway::class);
+        $paymentTransactionRepository  = $this->createMock(PaymentTransactionRepository::class);
+
+        $paymentService = new PaymentService($gateway, $paymentTransactionRepository);
+
+        $gateway->expects($this->atLeast(3))
+                ->method('pay')
+                ->will($this->onConsecutiveCalls(
+                    false, false, false
+                ));
+        
+        $paymentTransactionRepository->expects($this->never())
+                                     ->method('save');
+
+        $this->expectException(PaymentErrorException::class);
 
         $customer = $this->createMock(Customer::class);
         $item = $this->createMock(Item::class);
